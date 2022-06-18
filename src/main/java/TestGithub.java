b4659ac9163e7c8555b74f0fb3e94ec16de6beed
fanchuangqi <1053516554@qq.com>
2022-06-18 23:53:31 +0800

    Delete src/main/java directory

--- src/main/java/TestGithub.java
+++
@@ -1,7 +0,0 @@
-public class TestGithub {
-    public static void main(String[] args) {
-        System.out.println("提交代码");
-        System.out.println("------修改提交------");
-        System.out.println("*****分支合并******");
-    }
-}
